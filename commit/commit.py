39c721ad66bda6a828d747d797f226fd01b23af2
bobo.yang <bobo.yang@merico.dev>
2023-12-08 08:18:56 +0800

    Update file summaries in commit.py

--- commit/commit.py
+++ commit/commit.py
@@ -129,11 +129,6 @@ def get_file_summaries(modified_files, staged_files, user_input):
 
     # 在prompt中明确处置AI模型的输出格式需求
     normpath_summaries = gpt_file_summary(total_diff_decoded, modified_files, user_input)
-    print(f"""
-``` file summary
-{json.dumps(normpath_summaries, indent=4)}
-```
-    """)
 
     return normpath_summaries
 
