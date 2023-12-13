fb24069119acaa181c29623c6b76fc6f94402fce
bobo.yang <bobo.yang@merico.dev>
2023-12-13 10:57:08 +0800

    add ide service and group ui

--- libs/ui_utils/__init__.py
+++ libs/ui_utils/__init__.py
@@ -1,7 +1,8 @@
 from .iobase import parse_response_from_ui, pipe_interaction, pipe_interaction_mock
-from .multi_selections import ui_checkbox_select, CheckboxOption
-from .single_select import ui_radio_select, RadioOption
-from .text_edit import ui_text_edit
+from .multi_selections import ui_checkbox_select, CheckboxOption, make_checkbox_control
+from .single_select import ui_radio_select, RadioOption, make_radio_control
+from .text_edit import ui_text_edit, make_editor_control
+from .group import ui_group
 
 
 __all__ = [
@@ -13,4 +14,8 @@ __all__ = [
     "ui_text_edit",
     "CheckboxOption",
     "RadioOption",
+    "make_checkbox_control",
+    "make_radio_control",
+    "make_editor_control",
+    "ui_group"
 ]
