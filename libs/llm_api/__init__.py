dac115ed3294849a6707fb84e69c55eb2c651182
bobo <bobo.yang@merico.dev>
2024-01-31 16:14:51 +0800

    Refactor llm_api package imports and add new modules

--- libs/llm_api/__init__.py
+++ libs/llm_api/__init__.py
@@ -1,3 +1,26 @@
-from .openai import chat_completion_no_stream_return_json, chat_completion_stream, llm, llm_call_confirm, llm_edit_confirm, llm_func, llm_param
+from .openai import (
+	chat_completion_no_stream_return_json,
+	chat_completion_stream
+)
+from .chat import (
+	chat_json,
+	chat
+)
+from .text_confirm import llm_edit_confirm
+from .tools_call import llm_func, llm_param, chat_tools
+from .memory.base import ChatMemory
+from .memory.fixsize_memory import FixSizeChatMemory
 
-__all__ = ["chat_completion_stream", "chat_completion_no_stream_return_json", "llm", "llm_call_confirm", "llm_edit_confirm", "llm_func", "llm_param"]
+
+__all__ = [
+	"chat_completion_stream",
+	"chat_completion_no_stream_return_json",
+	"chat_json",
+	"chat",
+	"llm_edit_confirm",
+	"llm_func",
+	"llm_param",
+	"chat_tools",
+	"ChatMemory",
+	"FixSizeChatMemory"
+]
