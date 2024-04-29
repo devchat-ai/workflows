801c14634833cb9a28b4d3eb2a9123e20b5d9139
bobo <bobo.yang@merico.dev>
2024-04-29 11:06:30 +0800

    Fix tiktoken_util.py to set TIKTOKEN_CACHE_DIR dynamically

--- unit_tests/tools/tiktoken_util.py
+++ unit_tests/tools/tiktoken_util.py
@@ -1,5 +1,8 @@
+import os
 import tiktoken
 
+script_dir = os.path.dirname(os.path.realpath(__file__))
+os.environ['TIKTOKEN_CACHE_DIR'] = os.path.join(script_dir, 'tiktoken_cache')
 
 def get_encoding(encoding_name: str):
     """
