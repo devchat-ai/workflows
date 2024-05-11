ddd14faffa5567bdbf370ff390277bfd5d32b5ba
bobo <bobo.yang@merico.dev>
2024-05-11 15:07:50 +0800

    Refactor command.yml and rewrite.py for improved code readability and error handling

--- comments/main.py
+++ comments/main.py
@@ -5,23 +5,199 @@ import sys
 from devchat.ide.service import IDEService
 from devchat.ide.vscode_services import selected_lines, visible_lines
 from devchat.llm import chat
+from devchat.memory import FixSizeChatMemory
+
+PROMPT = """
+file: {file_path}
+```
+{selected_text}
+```
+"""
+
+PROMPT_ZH = """
+文件：{file_path}
+```
+{selected_text}
+```
+代码块中注释请使用中文描述。
+"""
+
+
+MESSAGES_FEW_SHOT = [
+    {
+        "role": "system",
+        "content": """
+You are a code assistant. Your task is to add comments for given code block.
+To add comments, you need to follow the following rules:
+1. don't change any code in code block, even space char;
+2. comment should be placed before the code line, don't append comment to the code line;
+3. keep comments already in code block;
+4. add comments for each code line;
+5. don't change string value in code block;
+
+there are a examples with correct outputs:
+input: 
+file: a1.py
+```
+# print("hello")
+
+print("Hello World") print("Hello World2")
+```
+output:
+```python
+# print("hello")
+
+# print Hello World
+print("Hello World") print("Hello World2")
+```
+In this example, "# print("hello")" is comment line, but we keep it in output.
+"print("Hello World") print("Hello World2")" is an error line, but we keep it in output,
+because output can't change any code, just insert comment lines to code block.
+
+
+here is an error example:
+```
+        // const filepath = document.uri.fsPath;
+        // const fileContent = document.getText();
+        // const posOffset = document.offsetAt(position);
+        // await outputAst(filepath, fileContent, posOffset);
+        // // await testTreesitterQuery(filepath, fileContent);
+        // const result2 = await findSimilarCodeBlock(filepath, fileContent, position.line, position.character);
+        // logger.channel()?.info("Result:", result2);
+        // if (1) {
+        //     return [];
+        // }
+
+        let response: CodeCompleteResult | undefined = undefined;
+```
+output is :
+```typescript
+        // init response
+        let response: CodeCompleteResult | undefined = undefined;
+```
+In this example, comments lines are missed, so this output is bad.
+
+here is an error example:
+```
+    const value = "open file";
+```
+output is :
+```typescript
+    // 设置变量值为打开文件
+    const value = "打开文件";
+```
+In this example, string in code is changed, so this output is bad.
+
+Output should format as code block.
+"""
+    },
+    {
+        "role": "user",
+        "content": """
+file: a1.py
+```
+    print("hello")
+```
+"""
+    },
+    {
+        "role": "assistant",
+        "content": """
+```python
+    # print hello
+    print("hello")
+```
+"""
+    },
+    {
+        "role": "user",
+        "content": """
+file: a1.py
+```
+    print("hell\\nworld")
+```
+"""
+    },
+    {
+        "role": "assistant",
+        "content": """
+```python
+    # print hello world
+    print("hell\\nworld")
+```
+"""
+    },
+    {
+        "role": "user",
+        "content": """
+file: t2.ts
+```
+                this.logEventToServer(
+                {
+                    completion_id: response!.id,
+                    type: "select",
+                    lines: response!.code.split('\\n').length,
+                    length: response!.code.length
+                });
+```
+"""
+    },
+    {
+        "role": "assistant",
+        "content": """
+```typescript
+                // log event to server
+                this.logEventToServer(
+                {
+                    // completion id
+                    completion_id: response!.id,
+                    // type of event
+                    type: "select",
+                    // number of lines in response code
+                    lines: response!.code.split('\\n').length,
+                    // length of response code
+                    length: response!.code.length
+                });
+```
+"""
+    },
+    {
+        "role": "user",
+        "content": """
+file: t2.ts
+```
+    // if (a == 1) {
+    //     console.log("a is 1");
+    // }
+
+    if (a == 2) {
+        console.log("a is 2");
+    }
+```
+"""
+    },
+    {
+        "role": "assistant",
+        "content": """
+```typescript
+    // if (a == 1) {
+    //     console.log("a is 1");
+    // }
+
+    // check if a is 2
+    if (a == 2) {
+        // log a is 2
+        console.log("a is 2");
+    }
+```
+"""
+    }
+]
 
 
 def get_selected_code():
-    """
-    Retrieves the selected lines of code from the user's selection.
-
-    This function extracts the text selected by the user in their IDE or text editor.
-    If no text has been selected, it prints an error message to stderr and exits the
-    program with a non-zero status indicating failure.
-
-    Returns:
-        dict: A dictionary containing the key 'selectedText' with the selected text
-        as its value. If no text is selected, the program exits.
-    """
+    """Retrieves the selected lines of code from the user's selection."""
     selected_data = IDEService().get_selected_range().dict()
-
-    miss_selected_error = "Please select some text."
     if selected_data["range"]["start"] == selected_data["range"]["end"]:
         readme_path = os.path.join(os.path.dirname(__file__), "README.md")
         if os.path.exists(readme_path):
@@ -29,101 +205,68 @@ def get_selected_code():
                 readme_text = f.read()
                 print(readme_text)
                 sys.exit(0)
-
-        print(miss_selected_error, file=sys.stderr, flush=True)
+        print("Please select some text.", file=sys.stderr, flush=True)
         sys.exit(-1)
-
     return selected_data
 
-
-def get_visible_code():
-    """
-    Retrieves visible code from the visible_lines function.
-
-    Returns:
-    visible_data: The visible code retrieved from the visible_lines function.
-    """
-    visible_data = IDEService().get_visible_range().dict()
-    return visible_data
-
-
-PROMPT = prompt = """
-Your task is:
-Add necessary line comments to the selected lines of code. Please keep in mind to ensure:
-1. Just add comments only for the selected portion of the code, \
-do not modify any thing beyond the selected portion;
-2. Add these comments above the corresponding lines of code;
-3. Output only the selected portion of the code with comments added;
-4. Maintains the same indentation as the selected portion of the code;
-5. Do not show any code beyond the selected portion;
-Following the task requirements, please ensure that the revised code segment \
-maintains the same indentation as the selected code to seamlessly integrate with \
-the existing code structure and maintain correct syntax.
-Here is the relevant context information for your reference:
-1. Selected portion of the code: {selected_text}
-2. Visible portion of the code: {visible_text}
-"""
-
-
-PROMPT_ZH = prompt = """
-你的任务是：
-使用中文给被选中的代码添加必要的注释。
-根据任务要求，仅修改被选中部分的代码。请确保：
-1. 仅修改被选中的代码，可见代码只是作为你理解的 context；
-2. 仅添加注释，代码结构保存不变；
-3. 修改后的代码用 markdown 代码块的格式输出；
-4. 修改后的代码段与所选代码保持相同的缩进，以与现有代码结构无缝集成并保持正确的语法；
-以下是你可以参考的 context 信息：
-1. 编辑器中被选中的代码：{selected_text}
-2. 当前编辑器中可见代码：{visible_text}
-"""
-
+memory = FixSizeChatMemory(max_size=20, messages=MESSAGES_FEW_SHOT)
 
 def get_prompt():
     ide_language = IDEService().ide_language()
     return PROMPT_ZH if ide_language == "zh" else PROMPT
 
-
-@chat(prompt=get_prompt(), stream_out=True)
+@chat(prompt=get_prompt(), stream_out=True, memory=memory)
 # pylint: disable=unused-argument
-def add_comments(selected_text, visible_text):
-    """
-    call ai to rewrite selected code
-    """
+def add_comments(selected_text, file_path):
+    """Call AI to rewrite selected code"""
     pass  # pylint: disable=unnecessary-pass
 
-
 def extract_markdown_block(text):
-    """
-    Extracts the first Markdown code block from the given text without the language specifier.
-
-    :param text: A string containing Markdown text
-    :return: The content of the first Markdown code block, or None if not found
-    """
+    """Extracts the first Markdown code block from the given text without the language specifier."""
     pattern = r"```(?:\w+)?\s*\n(.*?)\n```"
     match = re.search(pattern, text, re.DOTALL)
-
     if match:
         block_content = match.group(1)
         return block_content
     else:
+        if text.find("```"):
+            return None
         return text
 
+def remove_unnecessary_escapes(code_a, code_b):
+    code_copy = code_b  # Create a copy of the original code
+    escape_chars = re.finditer(r"\\(.)", code_b)
+    remove_char_index = []
+    for match in escape_chars:
+        before = code_b[max(0, match.start()-4):match.start()]
+        after = code_b[match.start()+1:match.start()+5]
+        substr = before + after
+        if substr in code_a:
+            remove_char_index.append(match.start())
+    remove_char_index.reverse()
+    for index in remove_char_index:
+        code_copy = code_copy[:index] + code_copy[index+1:]
+    return code_copy
 
 def main():
-    # prepare code
     selected_text = get_selected_code()
-    visible_text = get_visible_code()
+    file_path = selected_text.get("abspath", "")
+    code_text = selected_text.get("text", "")
 
-    # rewrite
-    response = add_comments(selected_text=selected_text, visible_text=visible_text)
-
-    # apply new code to editor
+    response = add_comments(selected_text=code_text, file_path=file_path)
     new_code = extract_markdown_block(response)
+
+    if not new_code:
+        ide_lang = IDEService().ide_language()
+        error_msg = (
+            "\n\nThe output of the LLM is incomplete and cannot perform code operations.\n\n"
+            if ide_lang != "zh" else "\n\n大模型输出不完整，不能进行代码操作。\n\n")
+        print(error_msg)
+        sys.exit(0)
+
+    new_code = remove_unnecessary_escapes(code_text, new_code)
     IDEService().diff_apply("", new_code)
-
     sys.exit(0)
-
 
 if __name__ == "__main__":
     main()
